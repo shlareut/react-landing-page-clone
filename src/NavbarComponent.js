da2b60fafbddd52ca8b7cba0e9f393fbfafa1442
Lars Reuter <lp.reuter@gmail.com>
2024-01-22 17:34:28 +0100

    Add navbar first draft

---
+++ src/NavbarComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './NavbarComponent.module.scss';
+
+export default function navbarComponent() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.navbar}>
+        <div className={styles.logo}>
+          <img
+            alt="hagrid-logo"
+            src="https://hagrid.io/assets/images/hagrid-logo-alt.svg"
+          />
+        </div>
+        <ul className={styles.linkList}>
+          <li className={styles.listItem}>Integrations</li>
+          <li className={styles.listItem}>Pricing</li>
+          <li className={styles.listItem}>Log In</li>
+          <li className={styles.listItem}>Try for Free</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import './index.css';
+import '@fontsource/be-vietnam-pro/400.css';
+import '@fontsource/be-vietnam-pro/500.css';
+import '@fontsource/be-vietnam-pro/600.css';
+import '@fontsource/be-vietnam-pro/700.css';
+import '@fontsource/be-vietnam-pro/800.css';
+import '@fontsource/be-vietnam-pro/900.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import styles from './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
