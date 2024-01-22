c8c9d076c4bc0b852ba03fbfbfe631065090b124
Lars Reuter <lp.reuter@gmail.com>
2024-01-22 21:11:40 +0100

    Add value prop boxes

--- src/ValuePropsComponent.js
+++ src/ValuePropsComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ValueProp from './ValueProp';
 import styles from './ValuePropsComponent.module.scss';
 
 export default function ValuePropsComponent() {
@@ -21,22 +22,52 @@ export default function ValuePropsComponent() {
           engagement
         </div>
         <div className={styles.valuePropsList}>
-          <div className={styles.valueProp}>
-            <img src="https://hagrid.io/assets/images/authentication.svg" />
-            <p className={styles.imageTitle}>Authenticated users</p>
-            <p className={styles.imageSubTitle}>
-              Socially authenticated posts. So no SPAM. Spend time on meaningful
-              questions from high-intent visitors.
-            </p>
-          </div>
-          <div className={styles.valueProp}>Box 2</div>
-          <div className={styles.valueProp}>Box 3</div>
-          <div className={styles.valueProp}>Box 4</div>
-          <div className={styles.valueProp}>Box 5</div>
-          <div className={styles.valueProp}>Box 6</div>
-          <div className={styles.valueProp}>Box 7</div>
-          <div className={styles.valueProp}>Box 8</div>
-          <div className={styles.valueProp}>Box 9</div>
+          <ValueProp
+            title="Authenticated users"
+            subTitle="Socially authenticated posts. So no SPAM. Spend time on meaningful
+        questions from high-intent visitors."
+            url="https://hagrid.io/assets/images/authentication.svg"
+          />
+          <ValueProp
+            title="Respect visitors privacy"
+            subTitle="Visitors can post anonymously, encouraging honest QnA and protecting their data."
+            url="https://hagrid.io/assets/images/data-security.svg"
+          />
+          <ValueProp
+            title="After they`ve left"
+            subTitle="Reach visitors, notify them of your responses, even after they have left your website. Even for anonymous posts!"
+            url="https://hagrid.io/assets/images/discussions.svg"
+          />
+          <ValueProp
+            title="Seed your questions"
+            subTitle="Don't wait till they ask. Publish your own FAQ, to kindle the conversation, and guide customers."
+            url="https://hagrid.io/assets/images/faq-base.svg"
+          />
+          <ValueProp
+            title="Automatic SEO"
+            subTitle="hagrid automatically feeds your QnA to the Google FAQ Schema, rank higher with zero effort."
+            url="https://hagrid.io/assets/images/seo.svg"
+          />
+          <ValueProp
+            title="Hyperintelligent Autocomplete"
+            subTitle="Get them to the answers, while they are asking the questions"
+            url="https://hagrid.io/assets/images/suggestions.svg"
+          />
+          <ValueProp
+            title="Powerful and flexible"
+            subTitle="Order questions as you like, respond in private, restrict answers to particular pages and anything else you can think of"
+            url="https://hagrid.io/assets/images/rocket.svg"
+          />
+          <ValueProp
+            title="Speak any language"
+            subTitle="We support all the major languages of the world. If we don't speak yours, we'll add it in 15 minutes."
+            url="https://hagrid.io/assets/images/globe.svg"
+          />
+          <ValueProp
+            title="Infinite customisation"
+            subTitle="Get hagrid to match your webpage brand and theme, down to the last pixel; without writing any code."
+            url="https://hagrid.io/assets/images/controls.svg"
+          />
         </div>
       </div>
     </div>
